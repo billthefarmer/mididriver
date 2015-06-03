1ebdd11fb61306007644729ecd25193921e4e7b5
Joseph Hindin <hindin@gmail.com>
2015-06-03 22:36:13 +0300

    sonivox is dynamically loaded at initialization time.
    There is no need to add libsonivox.so at link time, no need for
    pulled sonivox library and no need for custom Android.mk

--- mididriver/src/main/jni/midi.cpp
+++ mididriver/src/main/jni/midi.cpp
@@ -29,6 +29,22 @@
 #include "eas.h"
 #include "eas_reverb.h"
 
+#include <dlfcn.h>
+#include <android/log.h>
+
+// EAS function pointers
+EAS_PUBLIC const S_EAS_LIB_CONFIG *(*pEAS_Config) (void);
+EAS_PUBLIC EAS_RESULT (*pEAS_Init) (EAS_DATA_HANDLE *ppEASData);
+EAS_PUBLIC EAS_RESULT (*pEAS_SetParameter) (EAS_DATA_HANDLE pEASData, EAS_I32 module, EAS_I32 param, EAS_I32 value);
+EAS_PUBLIC EAS_RESULT (*pEAS_OpenMIDIStream) (EAS_DATA_HANDLE pEASData, EAS_HANDLE *pStreamHandle,
+						EAS_HANDLE streamHandle);
+EAS_PUBLIC EAS_RESULT (*pEAS_Shutdown) (EAS_DATA_HANDLE pEASData);
+EAS_PUBLIC EAS_RESULT (*pEAS_Render) (EAS_DATA_HANDLE pEASData, EAS_PCM *pOut, EAS_I32 numRequested,
+					EAS_I32 *pNumGenerated);
+EAS_PUBLIC EAS_RESULT (*pEAS_WriteMIDIStream)(EAS_DATA_HANDLE pEASData, EAS_HANDLE streamHandle, EAS_U8 *pBuffer,
+						EAS_I32 count);
+EAS_PUBLIC EAS_RESULT (*pEAS_CloseMIDIStream) (EAS_DATA_HANDLE pEASData, EAS_HANDLE streamHandle);
+
 // determines how many EAS buffers to fill a host buffer
 #define NUM_BUFFERS 4
 
@@ -47,7 +63,7 @@ Java_org_billthefarmer_mididriver_MidiDriver_init(JNIEnv *env,
     EAS_RESULT result;
 
     // get the library configuration
-    pLibConfig = EAS_Config();
+    pLibConfig = pEAS_Config();
     if (pLibConfig == NULL || pLibConfig->libVersion != LIB_VERSION)
 	return 0;
 
@@ -56,20 +72,20 @@ Java_org_billthefarmer_mididriver_MidiDriver_init(JNIEnv *env,
 	NUM_BUFFERS;
 
     // init library
-    if ((result = EAS_Init(&pEASData)) != EAS_SUCCESS)
+    if ((result = pEAS_Init(&pEASData)) != EAS_SUCCESS)
 	return 0;
 
     // select reverb preset and enable
-    EAS_SetParameter(pEASData, EAS_MODULE_REVERB, EAS_PARAM_REVERB_PRESET,
+    pEAS_SetParameter(pEASData, EAS_MODULE_REVERB, EAS_PARAM_REVERB_PRESET,
 		     EAS_PARAM_REVERB_CHAMBER);
-    EAS_SetParameter(pEASData, EAS_MODULE_REVERB, EAS_PARAM_REVERB_BYPASS,
+    pEAS_SetParameter(pEASData, EAS_MODULE_REVERB, EAS_PARAM_REVERB_BYPASS,
 		     EAS_FALSE);
 
     // open midi stream
-    if (result = EAS_OpenMIDIStream(pEASData, &midiHandle, NULL) !=
+    if (result = pEAS_OpenMIDIStream(pEASData, &midiHandle, NULL) !=
 	EAS_SUCCESS)
     {
-	EAS_Shutdown(pEASData);
+	pEAS_Shutdown(pEASData);
 	return 0;
     }
 
@@ -129,7 +145,7 @@ Java_org_billthefarmer_mididriver_MidiDriver_render(JNIEnv *env,
     count = 0;
     while (count < size)
     {
-	result = EAS_Render(pEASData, buffer + count,
+	result = pEAS_Render(pEASData, buffer + count,
 			    pLibConfig->mixBufferSize, &numGenerated);
 	if (result != EAS_SUCCESS)
 	    break;
@@ -159,7 +175,7 @@ Java_org_billthefarmer_mididriver_MidiDriver_write(JNIEnv *env,
     buf = (EAS_U8 *)env->GetByteArrayElements(byteArray, &isCopy);
     length = env->GetArrayLength(byteArray);
 
-    result = EAS_WriteMIDIStream(pEASData, midiHandle, buf, length);
+    result = pEAS_WriteMIDIStream(pEASData, midiHandle, buf, length);
 
     env->ReleaseByteArrayElements(byteArray, (jbyte *)buf, 0);
 
@@ -179,14 +195,107 @@ Java_org_billthefarmer_mididriver_MidiDriver_shutdown(JNIEnv *env,
     if (pEASData == NULL || midiHandle == NULL)
 	return JNI_FALSE;
 
-    if ((result = EAS_CloseMIDIStream(pEASData, midiHandle)) != EAS_SUCCESS)
+    if ((result = pEAS_CloseMIDIStream(pEASData, midiHandle)) != EAS_SUCCESS)
     {
-	EAS_Shutdown(pEASData);
+	pEAS_Shutdown(pEASData);
 	return JNI_FALSE;
     }
 
-    if ((result = EAS_Shutdown(pEASData)) != EAS_SUCCESS)
+    if ((result = pEAS_Shutdown(pEASData)) != EAS_SUCCESS)
 	return JNI_FALSE;
 
     return JNI_TRUE;
+}
+
+extern "C" {
+    jint JNI_OnLoad(JavaVM* vm, void* reserved);
+}
+
+jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+    JNIEnv* env;
+
+    if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
+        return -1;
+    }
+
+    jclass   dlExceptionClass = env->FindClass("org/billthefarmer/mididriver/DynamicLinkException");
+    if (dlExceptionClass == NULL) {
+        __android_log_write(ANDROID_LOG_ERROR, "org.billthefarmer.mididriver", "Failed to resolve "
+			    "org/billthefarmer/mididriver/DynamicLinkException");
+	    return -1;
+    }
+
+
+    void *libHandler;
+
+    __android_log_write(ANDROID_LOG_VERBOSE, "org.billthefarmer.mididriver", "Init function called" );
+
+    libHandler = dlopen("libsonivox.so", RTLD_LAZY);
+    if (!libHandler) {
+        env->ThrowNew(dlExceptionClass, "dlopen libsonivox.so failed" );
+        return -1;
+    } else {
+        __android_log_write(ANDROID_LOG_VERBOSE, "org.billthefarmer.mididriver", "dlopen libsonivox.so passed" );
+    }
+
+    pEAS_Config =  (EAS_PUBLIC const S_EAS_LIB_CONFIG *(*) (void))dlsym(libHandler, "EAS_Config");
+    if (!pEAS_Config) {
+        env->ThrowNew(dlExceptionClass, "EAS_Config resolution failed");
+        return -1;
+    }
+    
+    pEAS_Init = (EAS_PUBLIC EAS_RESULT (*) (EAS_DATA_HANDLE *ppEASData))dlsym(libHandler, "EAS_Init");
+    if (!pEAS_Config) {
+        env->ThrowNew(dlExceptionClass, "EAS_Init resolution failed");
+        return -1;
+    }
+      
+    pEAS_SetParameter = 
+      (EAS_PUBLIC EAS_RESULT (*) (EAS_DATA_HANDLE pEASData, EAS_I32 module, EAS_I32 param, EAS_I32 value))
+      dlsym(libHandler, "EAS_SetParameter");
+    if (!pEAS_SetParameter) {
+        env->ThrowNew(dlExceptionClass, "EAS_SetParameter resolution failed");
+        return -1;
+    }
+    
+    pEAS_OpenMIDIStream = 
+      (EAS_PUBLIC EAS_RESULT (*) (EAS_DATA_HANDLE pEASData, EAS_HANDLE *pStreamHandle, EAS_HANDLE streamHandle))
+      dlsym(libHandler, "EAS_OpenMIDIStream");
+    if (!pEAS_OpenMIDIStream) {
+        env->ThrowNew(dlExceptionClass, "EAS_OpenMIDIStream resolution failed");
+        return -1;
+    }
+
+    pEAS_Shutdown = (EAS_PUBLIC EAS_RESULT (*) (EAS_DATA_HANDLE pEASData))dlsym(libHandler, "EAS_Shutdown");
+    if (!pEAS_Shutdown) {
+        env->ThrowNew(dlExceptionClass, "EAS_Shutdown resolution failed");
+        return -1;
+    }
+
+    pEAS_Render =
+      (EAS_PUBLIC EAS_RESULT (*) (EAS_DATA_HANDLE pEASData, EAS_PCM *pOut, EAS_I32 numRequested,
+					     EAS_I32 *pNumGenerated))
+      dlsym(libHandler, "EAS_Render");
+    if (!pEAS_Render) {
+        env->ThrowNew(dlExceptionClass, "EAS_Render resolution failed");
+        return -1;
+    }
+    
+    pEAS_WriteMIDIStream = (EAS_PUBLIC EAS_RESULT (*)(EAS_DATA_HANDLE pEASData, EAS_HANDLE streamHandle, EAS_U8 *pBuffer,
+						      EAS_I32 count))
+      dlsym(libHandler, "EAS_WriteMIDIStream");
+    if (!pEAS_WriteMIDIStream) {
+        env->ThrowNew(dlExceptionClass, "EAS_WriteMIDIStream resolution failed");
+        return -1;
+    }
+
+    pEAS_CloseMIDIStream = (EAS_PUBLIC EAS_RESULT (*) (EAS_DATA_HANDLE pEASData, EAS_HANDLE streamHandle))
+      dlsym(libHandler, "EAS_CloseMIDIStream");
+    if (!pEAS_CloseMIDIStream) {
+        env->ThrowNew(dlExceptionClass, "EAS_CloseMIDIStream resolution failed");
+        return -1;
+    }
+
+    __android_log_write(ANDROID_LOG_VERBOSE, "org.billthefarmer.mididriver", "Init function passed" );
+    return JNI_VERSION_1_6;
 }
