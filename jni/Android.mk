77866a66ad64b0055abbc4bbc128f34a45120eb6
Bill Farmer <williamjfarmer@yahoo.co.uk>
2016-10-07 13:09:31 +0100

    Remove redundant parsers

--- jni/Android.mk
+++ jni/Android.mk
@@ -24,31 +24,23 @@ LOCAL_SRC_FILES = \
 	lib_src/eas_flog.c \
 	lib_src/eas_ima_tables.c \
 	lib_src/eas_imaadpcm.c \
-	lib_src/eas_imelody.c \
-	lib_src/eas_imelodydata.c \
 	lib_src/eas_math.c \
 	lib_src/eas_mdls.c \
 	lib_src/eas_midi.c \
 	lib_src/eas_mididata.c \
 	lib_src/eas_mixbuf.c \
 	lib_src/eas_mixer.c \
-	lib_src/eas_ota.c \
-	lib_src/eas_otadata.c \
 	lib_src/eas_pan.c \
 	lib_src/eas_pcm.c \
 	lib_src/eas_pcmdata.c \
 	lib_src/eas_public.c \
 	lib_src/eas_reverb.c \
 	lib_src/eas_reverbdata.c \
-	lib_src/eas_rtttl.c \
-	lib_src/eas_rtttldata.c \
+	lib_src/eas_voicemgt.c \
 	lib_src/eas_smf.c \
 	lib_src/eas_smfdata.c \
-	lib_src/eas_voicemgt.c \
 	lib_src/eas_wtengine.c \
 	lib_src/eas_wtsynth.c \
-	lib_src/eas_xmf.c \
-	lib_src/eas_xmfdata.c \
 	lib_src/wt_22khz.c \
 	lib_src/jet.c \
 	host_src/eas_config.c \
@@ -56,20 +48,31 @@ LOCAL_SRC_FILES = \
 	host_src/eas_report.c
 
 # not using these modules
+#	lib_src/eas_imelody.c \
+#	lib_src/eas_imelodydata.c \
+#	lib_src/eas_ota.c \
+#	lib_src/eas_otadata.c \
+#	lib_src/eas_rtttl.c \
+#	lib_src/eas_rtttldata.c \
+#	lib_src/eas_xmf.c \
+#	lib_src/eas_xmfdata.c \
 #	host_src/eas_main.c \
 #	host_src/eas_wave.c \
 #	lib_src/eas_wavefile.c \
 #	lib_src/eas_wavefiledata.c \
 
-LOCAL_CFLAGS+= -O2 -D UNIFIED_DEBUG_MESSAGES -D EAS_WT_SYNTH \
-	-D _IMELODY_PARSER -D _RTTTL_PARSER -D _OTA_PARSER \
-	-D _XMF_PARSER -D NUM_OUTPUT_CHANNELS=2 \
+LOCAL_CFLAGS+= -O2 -std=gnu99 -D UNIFIED_DEBUG_MESSAGES \
+	-D EAS_WT_SYNTH -D NUM_OUTPUT_CHANNELS=2 \
 	-D _SAMPLE_RATE_22050 -D MAX_SYNTH_VOICES=64 \
 	-D _8_BIT_SAMPLES -D _FILTER_ENABLED \
 	-D DLS_SYNTHESIZER \
 	-D _REVERB_ENABLED
 
 # not using these options
+# -D _IMELODY_PARSER
+# -D _RTTTL_PARSER
+# -D _OTA_PARSER
+# -D _XMF_PARSER
 # -D _WAVE_PARSER
 # -D _IMA_DECODER (needed for IMA-ADPCM wave files)
 # -D _CHORUS_ENABLED
